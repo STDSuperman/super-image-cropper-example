921fd70e86a3faadfbabc4673a966e34dfa15b0c
STDSuperman <2750556766@qq.com>
2022-06-12 14:46:30 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css';
 import 'cropperjs/dist/cropper.css';
-import ReactCropperJs from './components/react-cropperjs'
+// import ReactCropperJs from './components/react-cropperjs'
 import PureCropperJs from './components/pure-cropperjs'
+import React from 'react';
 
 function App() {
   // return <ReactCropperJs></ReactCropperJs>
